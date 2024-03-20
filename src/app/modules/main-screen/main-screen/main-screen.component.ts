75e31c360de5d16a0d912cd1f5b14f44ed69620f
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-03-20 17:02:08 +0200

    main-screen-page: backgroud with unsplash

--- src/app/modules/main-screen/main-screen/main-screen.component.ts
+++ src/app/modules/main-screen/main-screen/main-screen.component.ts
@@ -1,8 +1,10 @@
 import {
+  BehaviorSubject,
   debounceTime,
   filter,
   map,
   Observable,
+  of,
   Subject,
   switchMap,
   tap,
@@ -12,6 +14,9 @@ import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../../../core/services/weather.service';
 import { IWeatherByNow } from '../../../shared/models/weather.model';
 import { FormControl, FormGroup } from '@angular/forms';
+import { UnsplashService } from '../../../core/services/unsplash.service';
+import { IPhotoInfo } from '../../../shared/models/photo.model';
+import { shuffleArray } from '../../../shared/utils/arrrays.utils';
 
 @Component({
   selector: 'app-main-screen',
@@ -19,16 +24,26 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrl: './main-screen.component.css',
 })
 export class MainScreenComponent implements OnInit {
-  constructor(private weatherService: WeatherService) {}
+  constructor(
+    private weatherService: WeatherService,
+    private unsplashService: UnsplashService
+  ) {}
 
   searchForm = new FormGroup({
     cityName: new FormControl(''),
   });
 
+  initialActivePhoto: IPhotoInfo = {
+    url: '/assets/background-img.jpg',
+    title: 'Welcome!',
+  };
+
   weather$$ = new Subject<IWeatherByNow>();
+  photos$$ = new BehaviorSubject<IPhotoInfo[] | null>(null);
+  activePhoto = new Observable<IPhotoInfo | null>();
 
   ngOnInit(): void {
-    // this.getWeatherInfoByInput();
+    this.activePhoto = of(this.initialActivePhoto);
   }
 
   public onCityNameInput() {
@@ -36,6 +51,9 @@ export class MainScreenComponent implements OnInit {
       .pipe(
         debounceTime(500),
         filter(() => !!this.searchForm.value.cityName),
+        tap((term) => {
+          this.getPhotosByCityName(term.cityName ?? '');
+        }),
         switchMap(() =>
           this.weatherService.getCurrentWeatherByName(
             this.searchForm.value.cityName ?? ''
@@ -49,6 +67,16 @@ export class MainScreenComponent implements OnInit {
         error: (error) => {
           console.error('Error while fetching weather: ', error);
         },
+      });
+  }
+
+  public getPhotosByCityName(cityName: string): void {
+    this.unsplashService
+      .getPhotos(cityName)
+      .subscribe((photos: IPhotoInfo[]) => {
+        const shuffledPhotos = shuffleArray(photos);
+        this.activePhoto = of(this.photos$$.getValue()?.[0]!);
+        this.photos$$.next(shuffledPhotos);
       });
   }
 }
