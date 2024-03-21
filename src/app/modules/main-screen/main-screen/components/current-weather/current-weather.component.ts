2ce646ae9dc84b97c32b2f6841fc7f41263f020f
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-03-21 15:26:31 +0200

    main-screen-page: ui, part 3, time implementation

--- src/app/modules/main-screen/main-screen/components/current-weather/current-weather.component.ts
+++ src/app/modules/main-screen/main-screen/components/current-weather/current-weather.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { WeatherService } from '../../../../../core/services/weather.service';
-import { UnsplashService } from '../../../../../core/services/unsplash.service';
+import { Component, Input } from '@angular/core';
 import { Subject } from 'rxjs';
+
 import { IWeatherByNow } from '../../../../../shared/models/weather.model';
 
 @Component({
@@ -9,14 +8,7 @@ import { IWeatherByNow } from '../../../../../shared/models/weather.model';
   templateUrl: './current-weather.component.html',
   styleUrl: './current-weather.component.css',
 })
-export class CurrentWeatherComponent implements OnInit {
-  constructor(
-    private weatherService: WeatherService,
-    private unsplashService: UnsplashService
-  ) {}
-
+export class CurrentWeatherComponent {
   @Input({ required: true, alias: 'weatherData' }) weather$$ =
     new Subject<IWeatherByNow>();
-
-  ngOnInit(): void {}
 }
