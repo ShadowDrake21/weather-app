50f99f3652f439b7ca7dcb675a731cb0eeefcdc6
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-03-21 13:19:57 +0200

    extract a new component from main-screen.component

--- src/app/modules/main-screen/main-screen.module.ts
+++ src/app/modules/main-screen/main-screen.module.ts
@@ -6,9 +6,10 @@ import { MainScreenComponent } from './main-screen/main-screen.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TimePipe } from '../../shared/pipes/time.pipe';
 import { ChangeUnitsMeasurePipe } from '../../shared/pipes/change-units-measure.pipe';
+import { CurrentWeatherComponent } from './main-screen/components/current-weather/current-weather.component';
 
 @NgModule({
-  declarations: [MainScreenComponent],
+  declarations: [MainScreenComponent, CurrentWeatherComponent],
   imports: [
     CommonModule,
     MainScreenRoutingModule,
