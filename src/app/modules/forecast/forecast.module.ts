b691ed134b013baa8e9bf47d10e359389baa1dc9
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-03-24 23:59:57 +0200

    forecast slider, part 1

--- src/app/modules/forecast/forecast.module.ts
+++ src/app/modules/forecast/forecast.module.ts
@@ -5,9 +5,10 @@ import { ForecastRoutingModule } from './forecast-routing.module';
 import { ForecastComponent } from './forecast/forecast.component';
 import { ForecastItemComponent } from './forecast/components/forecast-item/forecast-item.component';
 import { ChangeUnitsMeasurePipe } from '../../shared/pipes/change-units-measure.pipe';
+import { ForecastSliderComponent } from './forecast/components/forecast-slider/forecast-slider.component';
 
 @NgModule({
-  declarations: [ForecastComponent, ForecastItemComponent],
+  declarations: [ForecastComponent, ForecastItemComponent, ForecastSliderComponent],
   imports: [CommonModule, ForecastRoutingModule, ChangeUnitsMeasurePipe],
 })
 export class ForecastModule {}
