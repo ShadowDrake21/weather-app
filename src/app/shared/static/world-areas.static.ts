b7ec17a1a963310956c63ac7785a55c08792cbf2
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-03-28 09:39:11 +0200

    clocks: functionality + ui, part 2

--- src/app/shared/static/world-areas.static.ts
+++ src/app/shared/static/world-areas.static.ts
@@ -4,53 +4,29 @@ export const europeAreas: IWorldAreas = {
   areaName: 'Europe',
   countriesCapitals: [
     'Amsterdam',
+    'Athens',
+    'Belgrade',
+    'Bratislava',
+    'Brussels',
     'Berlin',
     'Budapest',
+    'Copenhagen',
+    'Dublin',
     'Kiev',
+    'Lisbon',
     'London',
+    'Luxembourg',
     'Madrid',
     'Minsk',
+    'Monaco',
+    'Moscow',
+    'Oslo',
     'Paris',
+    'Prague',
+    'Rome',
+    'Sofia',
+    'Stockholm',
+    'Vienna',
     'Warsaw',
   ],
-};
-
-export const americasAreas: IWorldAreas = {
-  areaName: 'America',
-  countriesCapitals: [
-    'Buenos Aires',
-    'Brasilia',
-    'Ottawa',
-    'Tegucigalpa',
-    'Mexico City',
-    'Panama',
-    'Washington',
-    'Montevideo',
-    'Caracas',
-  ],
-};
-
-export const asianAreas: IWorldAreas = {
-  areaName: 'Asia',
-  countriesCapitals: [
-    'Kabul',
-    'Baku',
-    'Beijing',
-    'Tbilisi',
-    'New Delhi',
-    'Jakarta',
-    'Tehran',
-    'Seoul',
-    'Damascus',
-    'Dushanbe',
-    'Bangkok',
-    'Ashgabat',
-    'Abu Dhabi',
-    'Tashkent',
-  ],
-};
-
-export const africanAreas: IWorldAreas = {
-  areaName: 'Africa',
-  countriesCapitals: ['Abuja', 'Accra', 'Bangui', 'Brazzaville', 'Cairo'],
 };
