9cb634436b4562746943b658eeb1081f02c41a65
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-04-02 19:31:03 +0300

    world area change

--- src/app/shared/static/world-areas.static.ts
+++ src/app/shared/static/world-areas.static.ts
@@ -3,21 +3,15 @@ import { IWorldAreas } from '../models/world-areas.model';
 export const europeAreas: IWorldAreas = {
   areaName: 'Europe',
   countriesCapitals: [
-    'Amsterdam',
     'Athens',
-    'Belgrade',
     'Brussels',
     'Berlin',
     'Budapest',
-    'Dublin',
     'Kiev',
     'London',
     'Madrid',
     'Minsk',
-    'Monaco',
     'Moscow',
-    'Stockholm',
-
     'Warsaw',
   ],
 };
